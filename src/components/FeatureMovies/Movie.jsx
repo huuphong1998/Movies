3c4f6a7e2fa6e53492d58f8f06a695edffa82cac
Huu Phong <nghuuphong1998@gmail.com>
2025-08-21 11:16:21 +0700

    call api to build ui

--- src/components/FeatureMovies/Movie.jsx
+++ src/components/FeatureMovies/Movie.jsx
@@ -1,37 +1,32 @@
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const Movie = ({ data }) => {
+    if (!data) return null;
 
-const Movie = () => {
+    const { backdrop_path, title, release_date, overview } = data;
+
     return (
         <>
             <img
-                src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/VN-vi-20250721-TRIFECTA-perspective_5409932f-3102-4302-a97a-edec29cf8909_large.jpg"
+                src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
                 className="aspect-video object-cover brightness-50"
             />
             <div className="absolute bottom-[10%] left-8 w-1/2 sm:w-1/3">
-                <p className="mb-2 font-bold sm:text-[2vw]">Inside Out 2</p>
+                <p className="mb-2 font-bold sm:text-[2vw]">{title}</p>
                 <div>
-                    <p className="mb-1 inline-block border border-gray-400 p-1 text-gray-400">PG13</p>
-                    <p className="text-[1.2vw]">2025-08-08</p>
+                    <p className="text-[1.2vw]">{release_date}</p>
                 </div>
                 <div>
                     <div className="mt-4 hidden text-[1.2vw] sm:block">
                         <p className="mb-2 font-bold">Overview</p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis consequatur voluptatum
-                            quibusdam asperiores eos consequuntur? Perferendis dolor cupiditate ad distinctio, sint
-                            inventore ullam, labore tempore dolore error nisi delectus quis?
-                        </p>
+                        <p>{overview}</p>
                     </div>
-                    <div className="mt-4">
-                        <button className="text-10 mr-2 rounded bg-white px-4 py-2 text-black lg:text-lg">
-                            <FontAwesomeIcon icon={faPlay} /> Trailer
-                        </button>
-                        <button className="text-10 rounded bg-white/35 px-4 py-2 lg:text-lg">View Detail</button>
-                    </div>
+                </div>
+                <div className="mt-4">
+                    <button className="text-10 mr-2 rounded bg-white px-4 py-2 text-black lg:text-lg">Trailer</button>
+                    <button className="text-10 rounded bg-white/35 px-4 py-2 lg:text-lg">View Detail</button>
                 </div>
             </div>
         </>
     );
 };
+
 export default Movie;
--- src/components/FeatureMovies/index.jsx
+++ src/components/FeatureMovies/index.jsx
@@ -1,12 +1,39 @@
 import PaginateIndicator from './PaginateIndicator';
 import Movie from './Movie';
+import { useEffect, useState } from 'react';
 
 const FeatureMovies = () => {
+    const [movies, setMovies] = useState([]);
+    const [activeMovieId, setActiveMovieId] = useState();
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular', {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization:
+                    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDRiMDJjYzYzYzY2OTVhZGE5NGpmMGYzZjIyZjM3MSIsInN1YiI6IjY3NjA4ODNjMzhlY2QzYTZiNzZiYzBiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8B90BWYv6IGJLKt713PDR0Wn309IgwEahjo1uOEUWSg',
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                const popularMovies = data.results.slice(0, 4);
+                setMovies(popularMovies);
+                setActiveMovieId(popularMovies[0].id);
+            });
+    }, []);
+    console.log(movies);
+
     return (
         <div className="relative text-white">
-            <Movie />
-            <PaginateIndicator />
+            {movies
+                .filter(movie => movie.id === activeMovieId)
+                .map(movie => (
+                    <Movie key={movie.id} data={movie} />
+                ))}
+            <PaginateIndicator movies={movies} activeMovieId={activeMovieId} setActiveMovieId={setActiveMovieId} />
         </div>
     );
 };
+
 export default FeatureMovies;
